86c6f92ed5261a9882fcce28bae4f2a7de2fe640
chmcs2026shyamsundargupta-sudo <chmcs2026.shyamsundar.gupta@gmail.com>
2026-03-09 23:05:55 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://online-food-ordering-xh3v.onrender.com/orders",
 })
 export default api
